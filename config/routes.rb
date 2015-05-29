9ec1e3c7341c449c53aa456528fd0f98aa7ea864
Rebekah Chang <rchang@primerevenue.com>
2015-05-29 17:33:11 -0400

    Added StaticPages controller and home work about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MyRailsWebPage::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/work"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
