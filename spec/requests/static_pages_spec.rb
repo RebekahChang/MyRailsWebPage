291e3aa037ac19a3e2aacc0449969a2837e74004
Rebekah Chang <rchang@primerevenue.com>
2015-06-02 12:11:55 -0400

    added much css and precompiled for heroku

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,44 +2,53 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	subject { page }
+
   describe "Home page" do
 
-    it "should have the h1 'Welcome to my First Ruby Web Application!'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Welcome to my First Ruby Web Application!')
-    end
+  	before { visit root_path }
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Rebekah Chang")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector('h1', text: 'Welcome to my First Ruby Web Application!') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector'title', text: '| Home' }
   end
 
   describe "About page" do
+
+  	before { visit about_path }
+
   	it "should have content 'About me'" do
-  	  visit '/static_pages/about'
   	  page.should have_content('About me')
   	end
     it "should have title 'About'" do
-    	visit '/static_pages/about'
     	page.should have_selector('title', :text => "Rebekah Chang | About")
     end
   end
 
   describe "Work page" do
-  	it "shoul have content 'Work Experiences'" do
-  		visit '/static_pages/work'
+
+  	before { visit work_path }
+
+  	it "should have content 'Work Experiences'" do
   		page.should have_content('Work Experiences')
   	end
+
     it "should have title 'Work'" do
-    	visit '/static_pages/work'
     	page.should have_selector('title', :text => "Rebekah Chang | Work")
     end
   end
+
+  describe "Contact page" do
+
+  	before { visit contact_path }
+
+  	it "should have title 'Contact'" do
+  		page.should have_selector('title', :text => "Rebekah Chang | Contact")
+  	end
+
+  	it "should have content 'Contact me!'" do
+  		page.should have_content('Contact me!')
+  	end
+  end
+
 end
